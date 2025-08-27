f9b94d7792647fe3739cd6187e67450b6c8abf09
Sergio Garcia <sergio.garcia@sofka.com.co>
2025-08-27 14:51:13 -0500

    FIX: fix spotify service touse authentication

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StandardizeApiResponse } from '../models/api.models';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,20 @@ import { StandardizeApiResponse } from '../models/api.models';
 export class SpotifyService {
   private apiUrl = 'http://localhost:8080/api/v1/spotify';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
 
   getMarkets(): Observable<StandardizeApiResponse<string[]>> {
-    return this.http.get<StandardizeApiResponse<string[]>>(this.apiUrl);
+    return this.http.get<StandardizeApiResponse<string[]>>(
+      this.apiUrl,
+      { headers: this.getAuthHeaders() }
+    );
   }
 }
